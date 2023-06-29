d9b426bd3f28ce02a3f619f71f85e4ab38db0810
Shun Yao <shunyao643@gmail.com>
2023-06-30 00:31:27 +0800

    Rewrite show_results to calculate_metrics and return results in a dictionary

--- helper_functions.py
+++ helper_functions.py
@@ -1,23 +1,21 @@
 from sklearn.metrics import precision_score, recall_score, confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+import matplotlib
 
 
-def custom_metric(y_true, y_pred):
+def custom_metric(y_true, y_pred, custom_weight=0.7):
     precision = precision_score(y_true, y_pred)
     recall = recall_score(y_true, y_pred)
-
-    # Define the weight for the custom metric
-    custom_weight = 0.7
 
     combined_score = (custom_weight * recall) + ((1 - custom_weight) * precision)
     return combined_score
 
 
-def show_results(actual_result, predicted_result, labels):
-    cm = confusion_matrix(actual_result, predicted_result, labels=labels)
-    disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = labels)
-    disp.plot()
-    plt.show()
-    print(f"Accuracy\t: {accuracy_score(actual_result, predicted_result):.4}")
-    print(f"Precision\t: {precision_score(actual_result, predicted_result):.4}")
-    print(f"Recall\t\t: {recall_score(actual_result, predicted_result):.4}")
-    print(f"Score\t\t: {custom_metric(actual_result, predicted_result):.4}")
+def calculate_metrics(actual_result, predicted_result):
+    results = {
+        "Accuracy": accuracy_score(actual_result, predicted_result),
+        "Precision": precision_score(actual_result, predicted_result),
+        "Recall": recall_score(actual_result, predicted_result),
+        "Score": custom_metric(actual_result, predicted_result)
+    }
+    print(results)
+    return results
